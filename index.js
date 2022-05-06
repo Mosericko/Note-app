4c293f5163739b08af08a9bdc5b3d3f86fe47eda
Mosericko <mosericko@gmail.com>
2022-05-07 00:14:01 +0300

    Complete Project

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const modal = document.querySelector(".modal");
 const title = document.querySelector("input");
 const desc = document.querySelector("textarea");
 const addBtn = document.querySelector(".submit");
+const cancel = document.querySelector(".title i");
+const modalHeading = document.querySelector(".title p");
 
 const months = [
   "January",
@@ -22,15 +24,94 @@ const months = [
 
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
+let isUpdated = false,
+  updateId;
+// show notes
+function showAllNotes() {
+  document.querySelectorAll(".note-container").forEach((note) => note.remove());
+  notes.forEach((note, index) => {
+    let noteCard = `<div class="note-container">
+                      <div class="note-content">
+                        <div class="note-title">${note.title}</div>
+                        <div class="note-description">
+                        ${note.description}
+                        </div>
+                      </div>
+                      <div class="note-footer">
+                        <div class="date">${note.date}</div>
+                        <div class="menu">
+                          <i onclick= "showMenu(this)"class="uil uil-ellipsis-h"></i>
+                          <ul class="menu-items">
+                            <li onclick="updateNote(${index}, '${note.title}', '${note.description}' )"><i class="uil uil-edit-alt"></i> Edit</li>
+                            <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i> Delete</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>`;
+
+    addNote.insertAdjacentHTML("afterend", noteCard);
+  });
+}
+showAllNotes();
+
 addNote.addEventListener("click", () => {
+  title.focus();
   modalContainer.classList.add("show");
 });
 
+//clicking the X icon
+cancel.addEventListener("click", () => {
+  isUpdated = false;
+  title.value = "";
+  desc.value = "";
+  addBtn.value = "Save";
+  modalHeading.innerText = "Add Note";
+  modalContainer.classList.remove("show");
+});
+
+//clicking outside the modal
 window.onclick = (e) => {
   if (e.target == modalContainer) {
+    title.value = "";
+    desc.value = "";
+    addBtn.value = "Save";
+    modalHeading.innerText = "Add Note";
     modalContainer.classList.remove("show");
   }
 };
+
+//drop-down menu
+function showMenu(elem) {
+  elem.parentElement.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != elem) {
+      elem.parentElement.classList.remove("show");
+    }
+  });
+}
+
+//delete note
+function deleteNote(noteId) {
+  notes.splice(noteId, 1);
+
+  //update the collection in local storage
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  //show the updated notes
+  showAllNotes();
+}
+
+//update note
+function updateNote(noteId, noteTitle, description) {
+  isUpdated = true;
+  updateId = noteId;
+  addNote.click();
+  title.value = noteTitle;
+  desc.value = description;
+  addBtn.value = "Update Note";
+  modalHeading.innerText = "Edit Note";
+  console.log(noteId, title, desc);
+}
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -49,8 +130,14 @@ addBtn.addEventListener("click", (e) => {
       description: descValue,
       title: titleValue,
     };
-
-    notes.push(noteInfo);
+    if (!isUpdated) {
+      notes.push(noteInfo);
+    } else {
+      isUpdated = false;
+      notes[updateId] = noteInfo; //updating specified note
+    }
     localStorage.setItem("notes", JSON.stringify(notes));
+    cancel.click();
+    showAllNotes();
   }
 });
